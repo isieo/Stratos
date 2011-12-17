017bc6a72869afcc3033eb6457bfe139dfe47460
Calvin Tee <calvin@alphamelon.com>
2011-12-17 15:28:43 +0800

    user and grou[

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,4 @@
 class Report < ActiveRecord::Base
+  belongs_to :group
   has_many :report_items
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Group.find_or_create_by_name('Tech Team')
